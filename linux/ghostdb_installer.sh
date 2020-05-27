3e4caa46f3bc7228ccf686b55e844e875f349db0
jakekgrogan <jake.kgrogan@gmail.com>
2020-05-27 13:09:55 +0100

    Updated installer

--- linux/ghostdb_installer.sh
+++ linux/ghostdb_installer.sh
@@ -2,6 +2,7 @@
 
 wget https://ghostdb.github.io/linux/latest/ghostdb
 wget https://ghostdb.github.io/linux/ghostdb.service
+wget https://ghostdb.github.io/linux/ghostdbConf.json
 
 if [ grep -q "ghostdbservice" /etc/passwd ]; then 
     echo "ghostdbservice user already exists!";
@@ -15,9 +16,18 @@ else
     groupadd ghostdbservice && echo "Ghostdb group created!";
 fi
 
-cp ghostdb /bin/
-cp ghostdb.service /lib/systemd/system
+if [ -d "/etc/ghostdb" ]; then
+    echo "ghostdb config directory exists!"
+else
+    mkdir /etc/ghostdb && echo "ghostdb config directory created";
+    chown -R ghostdbservice:ghostdbservice /etc/ghostdb
+fi
+
+mv ghostdb /bin/
+mv ghostdb.service /lib/systemd/system
+mv ghostdbConf.json /etc/ghostdb
 chmod 755 /lib/systemd/system/ghostdb.service
+chmod 755 /etc/ghostdb/ghostdbConf.json
 chown -R ghostdbservice:ghostdbservice /home/ghostdbservice
 systemctl daemon-reload
 echo "Successfully reloaded daemons..."
